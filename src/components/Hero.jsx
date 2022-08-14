0faf54625005dcefa4980bcaa48ad7825a9f7c9d
anton <azvontsov@gmail.com>
2022-08-14 15:39:55 -0400

    !

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
           </div>
         </div>
 
-        <h1 className="font-poppins font-semibold ss:text-[68px] text-[40px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[36px] text-white ss:leading-[100.8px] leading-[75px] w-full">
           Телекоммуникаций
         </h1>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
